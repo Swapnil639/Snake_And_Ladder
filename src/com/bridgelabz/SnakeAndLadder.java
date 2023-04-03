72516e8855ff6e2cbd54b4204cefbc45d3805b40
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-03 20:06:41 +0530

    Added :Roll the dice get number betn 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,5 +4,9 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
        int player1=0;
        System.out.println("Player position :"+player1);
+        int dice= (int)(Math.random()*6+1);
+        System.out.println(dice);
+
+
     }
 }
