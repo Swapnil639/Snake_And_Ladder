39da3b99b7f4d7e487ff6496b1f41412015d6ad8
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-04 07:50:41 +0530

    Added :Player check for option they are no play,ladder and snake

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,11 +1,32 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
+    static final int noPlay = 0;
+    static final int ladder = 1;
+    static final int snake = 2;
+
     public static void main(String[] args) {
-       int player1=0;
-       System.out.println("Player position :"+player1);
-        int dice= (int)(Math.random()*6+1);
-        System.out.println(dice);
+        int StartPosition = 0;
+        System.out.println("Starting position :" + StartPosition);
+        int die = (int) (Math.random() * 10 % 6 + 1);
+        System.out.println("Roll the die to get a number is :" + die);
+        int optCheck = (int) (Math.random() * 10 % 3);
+
+        int nowYourPosition = 0;
+
+        if (optCheck == ladder) {
+            System.out.println("Ladder :");
+            nowYourPosition = nowYourPosition + die;
+        } else if (optCheck == snake) {
+            System.out.println("Snake :");
+            nowYourPosition = 0;
+        } else {
+            System.out.println("No play");
+            nowYourPosition = nowYourPosition;
+        }
+        System.out.println("Now your Position is :" + nowYourPosition);
 
 
     }
